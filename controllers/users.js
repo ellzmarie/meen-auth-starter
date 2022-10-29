665c33ac464e9451afbba7c45bff9b2d516d3010
ellzmarie <elliematutisdigal@gmail.com>
2022-10-29 11:38:38 -0700

    connects bcrypt

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,9 @@
 const express = require("express")
 const userRouter = express.Router()
 const User = require("../models/user.js")
+const bcrypt = require("bcrypt")
+const hashedString = bcrypt.hashSync("yourPasswordStringHere", bcrypt.genSaltSync(10))
+
 
 // New (registration page)
 
@@ -9,3 +12,11 @@ const User = require("../models/user.js")
 
 // Export User Router
 module.exports = userRouter
+
+userRouter.post("/", (req, res) => {
+    console.log(req.body)
+    //overwrite the user password with the hashed password, then pass that in to our database
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+    res.send(req.body)
+  })
+
--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema
 // User Schema
 const userSchema = Schema({
   email: { type: String, unique: true, required: true },
+//   username: {type: String, required: true},
   password: { type: String, required: true },
 })
 
